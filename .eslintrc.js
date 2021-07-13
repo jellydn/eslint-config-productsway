48d29e045b8753b753890f4e932b61a1255c0651
Huynh Duc Dung <dung@aircarbon.co>
2021-07-13 20:50:46 +0800

    :sparkles: Extend Airbnb's ESLint config with TypeScript and Prettier support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
   extends: ["airbnb-typescript", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
 };
