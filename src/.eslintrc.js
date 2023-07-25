478c60206a5d038bb34873dbe43f94e3b1c81da8
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-25 21:12:13 +0800

    refactor: migrate to xojs
    chore: add node engine

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
-  extends: ["airbnb-typescript", "prettier"],
+  extends: ["xo", "xo-typescript", "prettier"],
   parserOptions: {
     project: "./tsconfig.json",
   },
-  rules: {
-    "react/jsx-filename-extension": 0,
-    "import/extensions": 0,
-    "import/no-extraneous-dependencies": 0,
-  },
+  rules: {},
 };
