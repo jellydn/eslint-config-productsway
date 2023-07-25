a9398ceaa444b1e63655c94b351fbb077fb3b765
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2023-07-26 00:49:50 +0800

    feat: change some unsafe rules to warn (#178)

--- src/.eslintrc.ts.js
+++ src/.eslintrc.ts.js
@@ -3,5 +3,10 @@ module.exports = {
     parserOptions: {
         project: './tsconfig.json',
     },
-    rules: {},
+    rules: {
+        '@typescript-eslint/no-unsafe-assignment': 'warn',
+        '@typescript-eslint/no-unsafe-call': 'warn',
+        '@typescript-eslint/no-unsafe-member-access': 'warn',
+        '@typescript-eslint/no-unsafe-return': 'warn',
+    },
 };
